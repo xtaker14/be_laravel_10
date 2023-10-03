b3ec47393fb8380aa86d9affd39530c79d804598
Muhammad Febriyan Dwiki Prayoga <110667287+febriyandp12@users.noreply.github.com>
2023-10-03 16:27:28 +0700

    Update after_install.sh
    created after_install

--- after_install.sh
+++ after_install.sh
@@ -1,17 +1,14 @@
-#!/bin/bash
-## buat NPM
-export NVM_DIR="$HOME/.nvm" # export NVM
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-## buat change owner sama grub
-sudo chown ec2-user -R /var/www/html/tms-app ## buat change owner
-sudo chgrp ec2-user -R /var/www/html/tms-app ## buat change grub
-## cd ( change directory )
-cd /var/www/html/tms-app ## change directory ke tempat terkait
-## build sebelum run
-npm install
-npm install -g pm2
-npm run build ## buat build
-## stop dan start
-/home/ec2-user/.nvm/versions/node/v16.20.2/bin/pm2 delete all ## stop pm2
-/home/ec2-user/.nvm/versions/node/v16.20.2/bin/pm2 start ## start pm2
+cd /var/www/html/tms-app
+sudo chown ec2-user -R /var/www/html/tms-app
+sudo chgrp ec2-user -R /var/www/html/tms-app
+sudo chmod 0777 -R /var/www/html/tms-app/public
+sudo chmod 0777 -R /var/www/html/tms-app/storage
+php composer.phar update
+php composer.phar install
+php composer.phar dump-autoload -o
+php artisan route:cache
+php artisan config:clear & php artisan cache:clear
+php artisan migrate
+php artisan storage:link
+sudo service supervisord restart
+sudo service httpd restart

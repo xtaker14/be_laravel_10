dff26c51bba2d2ae5c70685e60174a63ce6dcdbf
Muhammad Febriyan Dwiki Prayoga <110667287+febriyandp12@users.noreply.github.com>
2023-10-05 01:08:37 +0700

    Update after_install.sh

--- after_install.sh
+++ after_install.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 cd /var/www/html/tms-app
 sudo chown ec2-user -R /var/www/html/tms-app
 sudo chgrp ec2-user -R /var/www/html/tms-app
@@ -7,9 +8,10 @@ php composer.phar update
 php composer.phar install
 php composer.phar dump-autoload -o
 php artisan optimize:clear
+php artisan view:clear
+php artisan config:cache
 php artisan route:cache
 php artisan config:clear & php artisan cache:clear
-php artisan view:clear
-php artisan migrate
 php artisan storage:link
+sudo service supervisord restart
 sudo service httpd restart

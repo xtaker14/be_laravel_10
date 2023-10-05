6a8f4913b9b5f6e348cc9e159068b2cc73b48743
Muhammad Febriyan Dwiki Prayoga <110667287+febriyandp12@users.noreply.github.com>
2023-10-05 14:31:27 +0700

    Update after_install.sh

--- after_install.sh
+++ after_install.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
+export NVM_DIR="$HOME/.nvm" # export NVM
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 cd /var/www/html/tms-app
 sudo chown ec2-user -R /var/www/html/tms-app
 sudo chgrp ec2-user -R /var/www/html/tms-app
-sudo chmod 0777 -R /var/www/html/tms-app/public
 sudo chmod 0777 -R /var/www/html/tms-app/storage
-php composer.phar update
 php composer.phar install
+npm install
 php composer.phar dump-autoload -o
 php artisan optimize:clear
 php artisan view:clear

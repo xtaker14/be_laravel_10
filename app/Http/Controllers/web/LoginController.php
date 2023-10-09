6a3d882bfc6d64c3c5cca24264ac1609223ec218
Alfain Fadil <dilalfain@gmail.com>
2023-10-09 13:24:04 +0700

    tes merge

--- app/Http/Controllers/web/LoginController.php
+++ app/Http/Controllers/web/LoginController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
-
+//tes merge
 class LoginController extends Controller
 {
     public function index()

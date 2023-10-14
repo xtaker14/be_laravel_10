f2bf74f3c5736373921f8117138366cbcf17da83
Muhammad Febriyan Dwiki Prayoga <110667287+febriyandp12@users.noreply.github.com>
2023-10-14 20:09:27 +0700

    Update after_install.sh

--- after_install.sh
+++ after_install.sh
@@ -5,6 +5,7 @@ export NVM_DIR="$HOME/.nvm" # export NVM
 cd /var/www/html/tms-app
 sudo chown ec2-user -R /var/www/html/tms-app
 sudo chgrp ec2-user -R /var/www/html/tms-app
+sudo chmod 0777 -R /var/www/html/tms-app/public
 sudo chmod 0777 -R /var/www/html/tms-app/storage
 php composer.phar install
 npm install
